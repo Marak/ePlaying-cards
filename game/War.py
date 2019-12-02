d36abad50a34ef0ab712458977d27a270beaca71
Justiniano-pagn <amanda.justiniano-pagn@intel.com>
2019-12-02 17:52:35 -0500

    Adding fixes.

--- game/War.py
+++ game/War.py
@@ -1,11 +1,22 @@
 # Copyright 2019 Amanda Justiniano amjustin@bu.edu
 
 from lib import Game
+from lib import CardDeck
 
 
 class War(Game):
     """Create an instance of the game War."""
-    def __init__(self):
+    def __init__(self, players=2):
+        """Initialize a War game object."""
+        super(War, self).__init__("standard", players)
 
+        # Create deck for the game
+        self.deck = CardDeck(namespace="../game/decks")
 
-    def rules(self):
+    def create_hand(self):
+        """Create Hands for Players."""
+        for player in range(self.players):
+            self.hands[player] = Hand()
+
+    def gameplay(self):
+        """Bulk of the game play should be implemented here.
